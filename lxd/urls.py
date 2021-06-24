883db19931fb7e98718aaaec6345a918e1d85a36
Joakim Hellsén <tlovinator@gmail.com>
2021-06-24 21:57:51 +0200

    Update urls.py

--- lxd/urls.py
+++ lxd/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path("images/", views.list_images, name="list_images"),
     # /image/ade188b16e5e719b73c14b5c19e7e65b8a15cf6f388be0bdb058012256b48a1e/
     path("image/<str:image_fingerprint>/", views.image_detail, name="image_detail"),
-    # /storage/
-    path("storage/", views.list_storage, name="list_storage"),
+    # /storage_pools/
+    path("storage_pools/", views.list_storage, name="list_storage"),
     # /storage/default/
     path("storage/<str:storage_name>/", views.storage_detail, name="storage_detail"),
     # /networks/
