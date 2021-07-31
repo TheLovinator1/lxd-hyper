1f6d3d344e2487d207d7e2c6bdfa219266410c28
Joakim Hellsén <tlovinator@gmail.com>
2021-08-01 00:10:36 +0200

    Change lxdhyper to lxd-hyper

--- lxdhyper/asgi.py
+++ lxdhyper/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for lxdhyper project.
+ASGI config for lxd-hyper project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- lxdhyper/wsgi.py
+++ lxdhyper/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lxdhyper project.
+WSGI config for lxd-hyper project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
